ff071f366b05e3564b20180a7bd0f6fbe5290a78
Brahm <brahm.kelly@gmail.com>
2022-05-26 09:44:25 -0400

    reading in from api, moved styles out

--- App.js
+++ App.js
@@ -1,77 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ActivityIndicator, ImageBackground, ScrollView, Image, } from 'react-native';
-import { useEffect, useState } from 'react';
+import { StatusBar } from "expo-status-bar";
+import {
+  
+  Text,
+  View,
+  ActivityIndicator,
+  ImageBackground,
+  ScrollView,
+  Image,
+} from "react-native";
+import { useEffect, useState } from "react";
+import styles from './src/styles'
 
-
-
-
-
-const img = {uri:'https://i.pinimg.com/originals/cc/1e/c6/cc1ec66bb88d96db9fdb9f5e11f11cd4.jpg'}
+const img = {
+  uri: "https://i.pinimg.com/originals/cc/1e/c6/cc1ec66bb88d96db9fdb9f5e11f11cd4.jpg",
+};
 
 export default function App() {
+  const [allRestaurants, setAllRestaurants] = useState();
 
-  const [allRestaurants, setAllRestaurants] = useState()
-
-  useEffect(  () => {
+  useEffect(() => {
     const getData = async () => {
-    const response = await fetch('https://my-first-firestore-bk.web.app/restaurants')
-    const data = await response.json()
-    setAllRestaurants(data);
-} 
-getData()}, []) 
-
-
-  
-  
+      const response = await fetch(
+        "https://my-first-firestore-bk.web.app/restaurants"
+      );
+      const data = await response.json();
+      setAllRestaurants(data);
+    };
+    getData();
+  }, []);
 
   return (
     <View style={styles.container}>
-      <ImageBackground style={styles.container} resizeMode='cover' source={img}>
-    
-    <ScrollView>
-      { allRestaurants ? (allRestaurants?.map((restaurant) => (
-        <>
-         <Text style={styles.text} key={restaurant.id}>{restaurant.name} </Text>
-        <Image 
-        source={{uri: restaurant.image}}
-         style={styles.image}/>
+      <ImageBackground style={styles.container} resizeMode="cover" source={img}>
+        <ScrollView>
+          {allRestaurants ? (
+            allRestaurants?.map((restaurant) => (
+              <>
+                <Text style={styles.text} key={restaurant.id}>
+                  {restaurant.name}{" "}
+                </Text>
+                <Image
+                  source={{ uri: restaurant.image }}
+                  style={styles.image}
+                />
+              </>
+            ))
+          ) : <ActivityIndicator size="large" color="pink" />
+          }
 
-        </>
-       
-      ))
-      ) : ( <ActivityIndicator size = 'large' color='pink'/> )
-  
-      
-      }
-
-      <StatusBar style="auto" />
-      </ScrollView>
-  
+          <StatusBar style="auto" />
+        </ScrollView>
       </ImageBackground>
     </View>
   );
-} 
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: '100%',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 
-  
- 
-
-  text: {
-    fontSize: 34,
-    marginVertical: 5,
-  }, 
-
-  image: {
-    width: '100%',
-    height: '100px',
-  }
-
-});
---
+++ src/styles/index.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+
+export default StyleSheet.create({
+    container: {
+      flex: 1,
+      width: "100%",
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  
+    text: {
+      fontSize: 34,
+      marginVertical: 0,
+      backgroundColor: "white",
+    },
+  
+    image: {
+      width: 400,
+      height: 100,
+      borderRadius: 10,
+    },
+  });
